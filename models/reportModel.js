c40a06dd95c5cc6d669e5033a003822a36235aa5
ajiajithkavi <ajithnamakkal02@gmail.com>
2025-05-23 11:01:25 +0530

    updated

--- models/reportModel.js
+++ models/reportModel.js
@@ -5,6 +5,15 @@ const ReportSchema = new mongoose.Schema({
   lead: { type: mongoose.Schema.Types.ObjectId, ref: 'Lead' },
   date: { type: Date, required: true },
   details: { type: String },
+  type: { type: String,
+    enum: ['Application', ' Inquiry'] 
+  },
+  status:{ type: String, 
+    enum: ['Approved', 'Pending', 'Rejected']
+  },
+  property: { type: String},
+  description: { type: String}
+
 });
 
 module.exports = mongoose.model('Report', ReportSchema);
